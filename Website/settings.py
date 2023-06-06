3b3bc5264753ad93da8986dccb10e0dab07755f1
NourEldin <noureldin.osama.saad@gmail.com>
2023-06-06 11:35:24 +0300

    Set DEBUG to True as default value in settings.py.

--- Website/settings.py
+++ Website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 LOCAL_IP = gethostbyname(gethostname())
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', LOCAL_IP]
 
