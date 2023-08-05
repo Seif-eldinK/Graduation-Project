033455da8a329c17f6c25a15b0023a9a568275e9
NourEldin <noureldin.osama.saad@gmail.com>
2023-08-05 18:29:40 +0300

    Refactor image generation process to be asynchronous, split into generate_image, get_status, and get_image methods, and update all corresponding routes and functions.

--- apps/image_generation/utils.py
+++ apps/image_generation/utils.py
@@ -1,19 +1,42 @@
-import requests
 import json
+
+import requests
 from django.conf import settings as django_settings
 
 url = django_settings.IMAGE_GENERATION_API_URL
 
 
 def generate_image(text_prompt: str, steps: int):
-    data = {
-        "text_prompt": text_prompt,
-        "steps": steps,
-    }
+    data = {"prompt": text_prompt, "num_inference_steps": steps, }
 
-    response = requests.post(url, json=data)
+    response = requests.post(url + "generate_image", json=data)
     try:
-        image_base64 = json.loads(response.content.decode('utf-8'))['image_base64']
+        task_id = json.loads(response.content.decode('utf-8'))['id']
     except Exception as error:
         raise Exception("Image Generation Server Error, Check the Server Link")
-    return image_base64
+    return task_id
+
+
+def get_status(task_id: int):
+    data = {"task_id": task_id}
+    response = requests.post(url + "get_status", json=data)
+    response = json.loads(response.content.decode('utf-8'))
+    try:
+        status = response['status']
+        if status == "Pending":
+            remaining_tasks = response['remaining_tasks']
+            return {"status": status, "remaining_tasks": remaining_tasks}
+    except Exception as error:
+        raise Exception("Image Generation Server Error, Check the Server Link")
+    return {"status": status}
+
+
+def get_image(task_id: int):
+    data = {"task_id": task_id}
+    response = requests.post(url + "get_image", json=data)
+    response = json.loads(response.content.decode('utf-8'))
+    try:
+        image_base64 = response['image_base64']
+    except Exception as error:
+        raise Exception("Image Generation Server Error, Check the Server Link")
+    return {"image_base64": image_base64}
--- apps/image_generation/views.py
+++ apps/image_generation/views.py
@@ -2,9 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.conf import settings as django_settings
 
-from .utils import generate_image
+from .utils import *
 
 
 @login_required
@@ -14,9 +13,22 @@ def image_generation(request):
 
 
 @api_view(['POST'])
-def image_generation_api(request):
-    # text_prompt = request.POST.get('text_prompt', '')  # form-encoded data
+def generate_image_api(request):
     text_prompt = request.data.get('text_prompt', '')  # json-encoded data
-    image_base64 = generate_image(text_prompt, django_settings.IMAGE_GENERATION_STEPS)
+    task_id = generate_image(text_prompt, django_settings.IMAGE_GENERATION_STEPS)
     print(f"{text_prompt = }")  # log to console
-    return Response({'text_prompt': text_prompt, 'image_base64': image_base64}, status=200)
+    return Response({'text_prompt': text_prompt, 'task_id': task_id}, status=200)
+
+
+@api_view(['POST'])
+def get_status_api(request):
+    task_id = request.data.get('task_id', '')
+    result = get_status(task_id)
+    return Response(result, status=200)
+
+
+@api_view(['POST'])
+def get_image_api(request):
+    task_id = request.data.get('task_id', '')
+    result = get_image(task_id)
+    return Response(result, status=200)
